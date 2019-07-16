e0a0f9f570ab91b78477d73fb62f9f7d0dc52ee7
Bron <34974437+bronweenie@users.noreply.github.com>
2019-07-16 10:57:30 +0100

    v1

---
+++ Laptop.ps1
@@ -0,0 +1,37 @@
+﻿set-executionpolicy unrestricted
+
+Function Detect-Laptop
+{
+Param( [string]$computer = “localhost” )
+$isLaptop = $false
+#The chassis is the physical container that houses the components of a computer. Check if the machine’s chasis type is 9.Laptop 10.Notebook 14.Sub-Notebook
+if(Get-WmiObject -Class win32_systemenclosure -ComputerName $computer | Where-Object { $_.chassistypes -eq 9 -or $_.chassistypes -eq 10 -or $_.chassistypes -eq 14})
+{ $isLaptop = $true }
+#Shows battery status , if true then the machine is a laptop.
+if(Get-WmiObject -Class win32_battery -ComputerName $computer)
+{ $isLaptop = $true }
+$isLaptop
+}
+
+
+If(Detect-Laptop) { 
+echo "You are using a laptop"
+echo "Getting device information..."
+echo "Writing to systems.txt"
+
+
+$un = echo $env:UserName
+$name = (get-WmiObject win32_computersystem).name
+$vendor = (get-WmiObject win32_computersystem).manufacturer
+$model = (get-WmiObject win32_computersystem).model
+$sn = (Get-WmiObject win32_bios).serialnumber
+
+
+
+Add-Content systems.txt "$un,$name,$vendor,$model,$sn"
+
+ }
+else {“You are not using a laptop...”}
+
+Read-Host -Prompt "Press Enter to exit"
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# tools
+# Daily Tools
+
+Just a place to keep tools/scripts that I use quite often
+Mainly Powershell
+
+### Laptop.ps1
+Gets username, device name, vendor, model and serial number
